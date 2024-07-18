98ac9e58d4eca559e25cf33df44cd93dc028e218
chris <christopheryim@hotmail.com>
2024-07-18 19:52:05 +0100

    Addition of layernorm before each of the multihead and feed forward layers

--- nanogpt-lecture/bigram.py
+++ nanogpt-lecture/bigram.py
@@ -5,7 +5,7 @@ from torch.nn import functional as F
 # Hyperparameters
 batch_size = 32  # how many sequences to run in parallel
 block_size = 8  # what is the max context length for prediction
-max_iters = 3000
+max_iters = 5000
 eval_interval = 300
 learning_rate = 1e-2
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -123,10 +123,13 @@ class Block(nn.Module):
         head_size = n_embd // n_head
         self.sa = MultiHeadAttention(n_head, head_size)
         self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
 
     def forward(self, x):
-        x = x + self.sa(x)
-        x = x + self.ffwd(x)
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+
         return x
 
 
@@ -142,6 +145,7 @@ class BigramLanguageModel(nn.Module):
             Block(n_embd, n_head=4),
             Block(n_embd, n_head=4),
             Block(n_embd, n_head=4),
+            nn.LayerNorm(n_embd),
         )
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
